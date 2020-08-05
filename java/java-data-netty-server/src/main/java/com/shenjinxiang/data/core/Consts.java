9cc99982f01685202a542845f369e306d303b08a
shenjinxiang <sjx-sword@163.com>
2020-08-05 23:10:38 +0800

    完善数据构建和命令行解析

--- java/java-data-netty-server/src/main/java/com/shenjinxiang/data/core/Consts.java
+++ java/java-data-netty-server/src/main/java/com/shenjinxiang/data/core/Consts.java
@@ -9,10 +9,10 @@ import io.netty.channel.Channel;
 public class Consts {
 
     public static int PORT;
-    public static int LENGTH = 20;
+    public static int LENGTH = 2048;
     public static int MIN_VAL = -1;
-    public static int MAX_VAL = 1;
-    public static int MIN_PEAK_VAL = 100;
+    public static int MAX_VAL = 5;
+    public static int MIN_PEAK_VAL = 110;
     public static int MAX_PEAK_VAL = 120;
 
     public static boolean ISSENDMSG = false;
