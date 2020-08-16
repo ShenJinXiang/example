67f7ef2d5eb020abb0cb45bd5aa572b1e092f12c
shenjinxiang <sjx-sword@163.com>
2020-08-16 16:42:47 +0800

    d

--- threejs/public/线框多面体/index.js
+++ threejs/public/线框多面体/index.js
@@ -3,40 +3,46 @@
     const scene = new THREE.Scene();
 
     // 创建模型
-    const geometry = new THREE.BoxGeometry(100, 100, 100);
-
-    //长方体 参数：长，宽，高
-    // var geometry = new THREE.BoxGeometry(100, 100, 100);
-    // 球体 参数：半径60  经纬度细分数40,40
-    // var geometry = new THREE.SphereGeometry(60, 40, 40);
-    // 圆柱  参数：圆柱面顶部、底部直径50,50   高度100  圆周分段数
-    // var geometry = new THREE.CylinderGeometry( 50, 50, 100, 25 );
-    // 正八面体
-    // var geometry = new THREE.OctahedronGeometry(50);
-    // 正十二面体
-    // var geometry = new THREE.DodecahedronGeometry(50);
-    // 正二十面体
-    // var geometry = new THREE.IcosahedronGeometry(50);
-
+    const geometry1 = new THREE.BoxGeometry(100, 100, 100);
+    // 创建模型
+    // const geometry2 = new THREE.BoxGeometry(50, 50, 50);
+    const geometry2 = new THREE.IcosahedronGeometry(50);
     // 材质对象
-    const meterial = new THREE.MeshLambertMaterial({
-        color: 0x0000FF
+    const meterial1 = new THREE.MeshLambertMaterial({
+        color: 0x0000FF,
+        opacity:0.7,
+        wireframe: true,
+        transparent:true
     });
-    const mesh = new THREE.Mesh(geometry, meterial);
+    const meterial2 = new THREE.MeshLambertMaterial({
+        color: 0xFF0000,
+        opacity:0.7,
+        wireframe: true,
+        transparent:true
+    });
+    const mesh1 = new THREE.Mesh(geometry1, meterial1);
+    const mesh2 = new THREE.Mesh(geometry2, meterial2);
+    mesh1.position.set(-150, 0, 0);
+    mesh2.position.set(150, 0, 0);
+
+    // 辅助坐标系  参数250表示坐标系大小，可以根据场景大小去设置
+    // const axisHelper = new THREE.AxisHelper(250);
+    const axisHelper = new THREE.AxesHelper(250);
+    scene.add(axisHelper);
     // 添加到场景
-    scene.add(mesh);
+    scene.add(mesh1);
+    scene.add(mesh2);
 
     // 点光源
     const point = new THREE.PointLight(0xFFFFFF);
-    // const point = new THREE.DirectionalLight(0xFFFFFF);
     // 点光源位置
     point.position.set(400, 200, 300);
     // 添加到场景
     scene.add(point);
 
     // 环境光
-    // const ambient = new THREE.AmbientLight();
-    // scene.add(ambient);
+    const ambient = new THREE.AmbientLight();
+    scene.add(ambient);
 
     // 相机设置
     let width = window.innerWidth,
@@ -46,7 +52,7 @@
         s = 200,
         camera = new THREE.OrthographicCamera(-s * k, s * k, s, -s, 1, 1000);
     // 相机位置
-    camera.position.set(200, 300, 200);
+    camera.position.set(100, 400, 400);
     // //设置相机方向(指向的场景对象)
     camera.lookAt(scene.position);
 
@@ -56,13 +62,15 @@
     // 背景色
     renderer.setClearColor(0xb9d3ff, 1);
     document.body.appendChild(renderer.domElement);
+
     let t0 = new Date().getTime();
     let animate = () => {
         let t1 = new Date().getTime(),
             t = t1 - t0;
         t0 = t1;
         // mesh.rotateX(0.001 * t);
-        mesh.rotateX(0.001 * t);
+        mesh1.rotateZ(0.001 * t);
+        mesh2.rotateZ(-0.002 * t);
         // 渲染
         renderer.render(scene, camera);
         requestAnimationFrame(animate);
