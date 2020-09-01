54e06bfd75009c845c709aff746660988e77b5c9
ShenJinXiang <sjx-sword@163.com>
2020-09-01 18:06:42 +0800

    调整底部链接样式

--- html/public/智企会计工作台/index.js
+++ html/public/智企会计工作台/index.js
@@ -8,32 +8,45 @@ $(() => {
             width: $("#tableGrid").parent().width(),
             // width: 800,
             datatype: 'local',
-            colNames: ['客户名称', '社会信用代码', '已填表', '已申报', '已缴款'],
+            colNames: ['客户名称', '社会信用代码', '未填表', '未申报', '未缴款'],
             colModel: [
                 {name: 'KHMC', index: 'KHMC', width: 200, sortable: false},
                 {name: 'SHXYDM', index: 'SHXYDM', width: 200, sortable: false},
                 {name: 'WTX', index: 'WTX', width: 100, sortable: false, align: 'center',
                     formatter: function (val, option, row){
-                        return '<div class="zt-box">' +
-                            '<span class="val-span">' + val + '</span> ' +
-                            '<i class="iconfont icon-List"></i>'+
-                            '</div>'
+                        let html = '<div class="zt-box">' ;
+                        if (val > 0) {
+                            html += '<span class="val-span red">' + val + '</span> ' +
+                                '<i class="iconfont icon-List"></i>';
+                        } else {
+                            html += '<span class="val-span green">' + val + '</span> ';
+                        }
+                        html += '</div>';
+                        return html;
                     }},
                 {name: 'WSB', index: 'WSB', width: 100, sortable: false, align: 'center',
                     formatter: function (val, option, row){
-                        return '<div class="zt-box">' +
-                            '<span class="val-span">' + val + '</span> ' +
-                            '<i class="iconfont icon-List"></i>'+
-                            '</div>'
-
+                        let html = '<div class="zt-box">' ;
+                        if (val > 0) {
+                            html += '<span class="val-span red">' + val + '</span> ' +
+                                '<i class="iconfont icon-List"></i>';
+                        } else {
+                            html += '<span class="val-span green">' + val + '</span> ';
+                        }
+                        html += '</div>';
+                        return html;
                     }},
                 {name: 'WJK', index: 'WJK', width: 100, sortable: false, align: 'center',
                     formatter: function (val, option, row){
-                        return '<div class="zt-box">' +
-                            '<span class="val-span">' + val + '</span> ' +
-                            '<i class="iconfont icon-List"></i>'+
-                            '</div>'
-
+                        let html = '<div class="zt-box">' ;
+                        if (val > 0) {
+                            html += '<span class="val-span red">' + val + '</span> ' +
+                                '<i class="iconfont icon-List"></i>';
+                        } else {
+                            html += '<span class="val-span green">' + val + '</span> ';
+                        }
+                        html += '</div>';
+                        return html;
                     }},
             ],
             prmNames: {
@@ -60,14 +73,14 @@ $(() => {
     function getData() {
         var data = [
             {KHMC: "山西亿美天成文化传媒有限公司", SHXYDM: "91140100MAOJR3YW8Q", "YTX": 5, "WTX": 3, "YSB": 5, "WSB": 3, "YJK": 6, "WJK": 2},
+            {KHMC: "山西亿美天成文化传媒有限公司", SHXYDM: "91140100MAOJR3YW8Q", "YTX": 5, "WTX": 0, "YSB": 5, "WSB": 3, "YJK": 6, "WJK": 2},
+            {KHMC: "山西亿美天成文化传媒有限公司", SHXYDM: "91140100MAOJR3YW8Q", "YTX": 5, "WTX": 3, "YSB": 5, "WSB": 3, "YJK": 6, "WJK": 2},
+            {KHMC: "山西亿美天成文化传媒有限公司", SHXYDM: "91140100MAOJR3YW8Q", "YTX": 5, "WTX": 0, "YSB": 5, "WSB": 0, "YJK": 6, "WJK": 2},
             {KHMC: "山西亿美天成文化传媒有限公司", SHXYDM: "91140100MAOJR3YW8Q", "YTX": 5, "WTX": 3, "YSB": 5, "WSB": 3, "YJK": 6, "WJK": 2},
             {KHMC: "山西亿美天成文化传媒有限公司", SHXYDM: "91140100MAOJR3YW8Q", "YTX": 5, "WTX": 3, "YSB": 5, "WSB": 3, "YJK": 6, "WJK": 2},
             {KHMC: "山西亿美天成文化传媒有限公司", SHXYDM: "91140100MAOJR3YW8Q", "YTX": 5, "WTX": 3, "YSB": 5, "WSB": 3, "YJK": 6, "WJK": 2},
             {KHMC: "山西亿美天成文化传媒有限公司", SHXYDM: "91140100MAOJR3YW8Q", "YTX": 5, "WTX": 3, "YSB": 5, "WSB": 3, "YJK": 6, "WJK": 2},
-            {KHMC: "山西亿美天成文化传媒有限公司", SHXYDM: "91140100MAOJR3YW8Q", "YTX": 5, "WTX": 3, "YSB": 5, "WSB": 3, "YJK": 6, "WJK": 2},
-            {KHMC: "山西亿美天成文化传媒有限公司", SHXYDM: "91140100MAOJR3YW8Q", "YTX": 5, "WTX": 3, "YSB": 5, "WSB": 3, "YJK": 6, "WJK": 2},
-            {KHMC: "山西亿美天成文化传媒有限公司", SHXYDM: "91140100MAOJR3YW8Q", "YTX": 5, "WTX": 3, "YSB": 5, "WSB": 3, "YJK": 6, "WJK": 2},
-            {KHMC: "山西亿美天成文化传媒有限公司", SHXYDM: "91140100MAOJR3YW8Q", "YTX": 5, "WTX": 3, "YSB": 5, "WSB": 3, "YJK": 6, "WJK": 2},
+            {KHMC: "山西亿美天成文化传媒有限公司", SHXYDM: "91140100MAOJR3YW8Q", "YTX": 5, "WTX": 3, "YSB": 5, "WSB": 3, "YJK": 6, "WJK": 0},
         ]
         return data;
     }
