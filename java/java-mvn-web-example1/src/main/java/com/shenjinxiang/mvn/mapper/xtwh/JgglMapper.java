24b27fb7053772d9058b459b801c199f9641e2f7
shenjinxiang <sjx-sword@163.com>
2020-09-03 07:31:51 +0800

    jggl list

---
+++ java/java-mvn-web-example1/src/main/java/com/shenjinxiang/mvn/mapper/xtwh/JgglMapper.java
@@ -0,0 +1,8 @@
+package com.shenjinxiang.mvn.mapper.xtwh;
+
+/**
+ * @Author: ShenJinXiang
+ * @Date: 2020/9/3 06:52
+ */
+public interface JgglMapper {
+}
---
+++ java/java-mvn-web-example1/src/main/java/com/shenjinxiang/mvn/services/JgglService.java
@@ -0,0 +1,8 @@
+package com.shenjinxiang.mvn.services;
+
+/**
+ * @Author: ShenJinXiang
+ * @Date: 2020/9/3 06:53
+ */
+public class JgglService {
+}
