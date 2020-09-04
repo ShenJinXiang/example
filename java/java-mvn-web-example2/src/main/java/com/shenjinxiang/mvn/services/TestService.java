3e79bd4405ae6a95e0e7c9f0b6b560bfd8d6136e
ShenJinXiang <sjx-sword@163.com>
2020-09-04 15:54:06 +0800

    Java-App-Jk-Kit

--- java/java-mvn-web-example2/src/main/java/com/shenjinxiang/mvn/services/TestService.java
+++ java/java-mvn-web-example2/src/main/java/com/shenjinxiang/mvn/services/TestService.java
@@ -2,7 +2,9 @@ package com.shenjinxiang.mvn.services;
 
 import com.shenjinxiang.mvn.mapper.TestMapper;
 import com.shenjinxiang.rapid.annotations.MyBatisDbConn;
+import com.shenjinxiang.rapid.annotations.MyBatisDbConnTx;
 import com.shenjinxiang.rapid.domain.Bean;
+import com.shenjinxiang.rapid.exceptions.BizException;
 import com.shenjinxiang.rapid.plugin.mybatis.MyBatisSessionManager;
 
 import java.util.List;
@@ -13,9 +15,12 @@ import java.util.List;
  */
 public class TestService {
 
-    @MyBatisDbConn
+    @MyBatisDbConnTx
     public List<Bean> queryAllZyList() {
         TestMapper testMapper = MyBatisSessionManager.getMapper(TestMapper.class);
+        if (true) {
+            throw new BizException("hahaha");
+        }
         List<Bean> list = testMapper.queryAllZyList();
         return list;
     }
