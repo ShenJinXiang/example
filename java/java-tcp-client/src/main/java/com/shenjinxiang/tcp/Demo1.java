5dd84388cf305063dcadbc8dd4c967683870e331
ShenJinXiang <sjx-sword@163.com>
2020-09-14 20:26:20 +0800

    解析数据

---
+++ java/java-tcp-client/src/main/java/com/shenjinxiang/tcp/Demo1.java
@@ -0,0 +1,11 @@
+package com.shenjinxiang.tcp;
+
+public class Demo1 {
+
+    public static void main(String[] args) {
+        int a = Integer.parseInt("30303030", 16);
+        System.out.println(a);
+        int b = Integer.parseInt("32303163", 16);
+        System.out.println(b);
+    }
+}
---
+++ java/java-tcp-client/src/main/java/com/shenjinxiang/tcp/Wave.java
@@ -0,0 +1,25 @@
+package com.shenjinxiang.tcp;
+
+import java.util.List;
+
+public class Wave {
+
+    private List<Float> data;
+    private int pickpoint;
+
+    public List<Float> getData() {
+        return data;
+    }
+
+    public void setData(List<Float> data) {
+        this.data = data;
+    }
+
+    public int getPickpoint() {
+        return pickpoint;
+    }
+
+    public void setPickpoint(int pickpoint) {
+        this.pickpoint = pickpoint;
+    }
+}
