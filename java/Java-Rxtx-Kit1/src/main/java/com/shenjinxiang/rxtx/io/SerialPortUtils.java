2a78e2ac384171570c6a4860faf20ca09002654a
ShenJinXiang <sjx-sword@163.com>
2020-09-27 17:02:15 +0800

    kit2添加data parser

--- java/Java-Rxtx-Kit1/src/main/java/com/shenjinxiang/rxtx/io/SerialPortUtils.java
+++ java/Java-Rxtx-Kit1/src/main/java/com/shenjinxiang/rxtx/io/SerialPortUtils.java
@@ -136,44 +136,12 @@ public class SerialPortUtils implements SerialPortEventListener {
                     ByteBuf buff1 = buff.readSlice(19);
                     byte[] message = new byte[buff1.readableBytes()];
                     buff1.readBytes(message);
-                    printData(message);
+                    DataParser.parse(message);
                 }
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
-
-    private void printData(byte[] bytes) {
-        String hex = ByteKit.byteArrayToHexStr(bytes);
-        StringBuffer stringBuffer = new StringBuffer();
-        stringBuffer.append("接收到数据：");
-        stringBuffer.append("\n\t字节数组：").append(Arrays.toString(bytes));
-        stringBuffer.append("\n\t十六进制：").append(hex);
-        String led1Str = ByteKit.getBit(bytes[4]);
-        String led2Str = ByteKit.getBit(bytes[5]);
-        String led3Str = ByteKit.getBit(bytes[6]);
-        String led4Str = ByteKit.getBit(bytes[7]);
-        String press1Str = ByteKit.getBit(bytes[8]);
-        String press2Str = ByteKit.getBit(bytes[9]);
-        String press3Str = ByteKit.getBit(bytes[10]);
-        String press4Str = ByteKit.getBit(bytes[11]);
-        byte[] bytes1 = new byte[] {0x00, 0x00, bytes[12], bytes[13]};
-        byte[] bytes2 = new byte[] {0x00, 0x00, bytes[14], bytes[15]};
-        stringBuffer.append("\n\tLED灯数据：")
-                .append(led1Str).append("   ")
-                .append(led2Str).append("    ")
-                .append(led3Str).append("    ")
-                .append(led4Str);
-        stringBuffer.append("\n\t按键数据：")
-                .append(press1Str).append("    ")
-                .append(press2Str).append("    ")
-                .append(press3Str).append("    ")
-                .append(press4Str);
-        stringBuffer.append("\n\t摇杆数据：")
-                .append(ByteArrayConveter.getInt(bytes1, 0)).append("    ")
-                .append(ByteArrayConveter.getInt(bytes2, 0));
-        logger.info(stringBuffer.toString());
     }
 
     /**
