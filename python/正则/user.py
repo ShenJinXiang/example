897b6e7cb195dcc4fbdf9628171dc9c0a6059bf9
shenjinxiang <sjx-sword@163.com>
2020-10-04 15:29:01 +0800

    d

--- python/正则/user.py
+++ python/正则/user.py
@@ -6,6 +6,8 @@ import re
 验证用户名是否正确
 用户名必须由字母、数字或下划线构成且长度在6~20个字符之间
 """
+
+
 def testUsername():
     reg = r'^[0-9A-Za-z_]{6,20}$'
     while True:
@@ -19,5 +21,6 @@ def testUsername():
             print('验证正确')
     print('程序结束...')
 
-if __name__ == '__main__' :
+
+if __name__ == '__main__':
     testUsername()
