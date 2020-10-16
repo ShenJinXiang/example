8624959e08068d278d57c568d7220bd90001f204
shenjinxiang <sjx-sword@163.com>
2020-10-17 07:49:16 +0800

    web

--- python/books/hard_way/p50/app.py
+++ python/books/hard_way/p50/app.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python3
 
 from flask import Flask
+from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+@app.route("/info")
+def info():
+    str1 = 'Hello World!'
+    return render_template("index.html", greeting=str1)
+
+@app.route("/hello")
+def hello():
+    name = request.args.get("name", "World!")
+    str1 = f'Hello {name}'
+    return render_template('index.html', greeting=str1)
 
 if __name__ == '__main__':
     app.run()
