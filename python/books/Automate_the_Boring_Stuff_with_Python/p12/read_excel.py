d9dd3e78732b1e3b6c2b73e9285bbeee79fbe9fa
shenjinxiang <sjx-sword@163.com>
2020-10-21 07:44:43 +0800

    sdf

---
+++ python/books/Automate_the_Boring_Stuff_with_Python/p12/read_excel.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import openpyxl as xl
+
+wb = xl.load_workbook("./example.xlsx")
+print(wb)
+print(type(wb))
+print(wb.sheetnames)
+
+# sheet_names = wb.get_sheet_names
+sheet1 = wb['Sheet1']
+print(f"sheet title: {sheet1}")
+print(sheet1.title)
+print(sheet1['A1'].value)
+
+for r in sheet1:
+    for c in r:
+        print(type(c.value), c.value)
+
+for n in range(10):
+    print('170426')
